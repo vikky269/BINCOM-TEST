4070476981239da91fd0f860108cee6aa0e12fe2
Amune victor <viktorefi2001@gmail.com>
2024-11-22 11:05:23 +0100

    implemented toggle nav

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-// script.js
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const nameField = document.getElementById('name');
-    const emailField = document.getElementById('email');
-    const messageField = document.getElementById('message');
+    event.preventDefault()
+    const nameField = document.getElementById('name')
+    const emailField = document.getElementById('email')
+    const messageField = document.getElementById('message')
 
     const name = nameField.value;
     const email = emailField.value;
     const message = messageField.value;
   
-    alert(`Thank you, ${name}! Your message has been received.`);
+    alert(`Thank you, ${name}! Your message has been received.`)
 
     
-    nameField.value = "";
-    emailField.value = "";
-    messageField.value = "";
+    nameField.value = ""
+    emailField.value = ""
+    messageField.value = ""
 });
+
+
+const menuToggle = document.getElementById("burger-toggle")
+const navMenu = document.getElementById("nav-menu")
+const navLinks = document.querySelectorAll("#nav-menu li a")
+
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active") 
+})
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      navMenu.classList.remove("active")
+    })
+  })
